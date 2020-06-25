fd379f7c112b5160edef61acd79ecd563b373d03
eoinraffs <Artur.Raibhfheartaigh@academytrainee.com>
2020-06-25 12:04:00 +0100

    Updated java code

--- src/com/qa_software1/QA_Software1_1.java
+++ src/com/qa_software1/QA_Software1_1.java
@@ -1,5 +1,15 @@
 package com.qa_software1;
 
 public class QA_Software1_1 {
-
+	
+	public static void myMethod(){
+		int num = 2020;
+		String year = "Is the best year yet!";
+		System.out.println(num + " "+ year);
+	}
+	
+	public static void main(String[] args) {
+	myMethod();
+	
+	}
 }
--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module qa_software1 {
-}
